ee1dd3f377dda241c13e57a749fd60c8b2ef6f98
Kai Chen <kchen2@e1z2r2p7.42.us.org>
2019-11-14 03:05:09 -0800

    folder

---
+++ incs/prin_rec.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prin_rec.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchen2 <kchen2@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/14 02:00:56 by kchen2            #+#    #+#             */
+/*   Updated: 2019/11/14 02:31:32 by kchen2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRIN_REC_H
+# define PRIN_REC_H
+
+int		main(int ac, char **av);
+int		ft_putchar(char c);
+
+void	ft_midsquare(int x);
+void	ft_lastline(int x);
+
+int		rush00(int x, int y);
+
+#endif
